271075ff1fb0c11b116ac0a9a14a29ad24e8be07
hira <hiraahsan94@gmail.com>
2019-12-17 12:09:30 -0500

    updating media queries and contact form, updating nav bar for mobile

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ AOS.init();
 AOS.init({
     disable: "mobile",
 })
+
+
+// $(document).ready(function () {
+//     $('.navMenu').click(function () {
+//         $('.navMenu').toggleClass('open')
+//         $('.sidebar').toggleClass('open')
+//     })
+//     $('header a').click(function () {
+//         $('.navMenu').removeClass('open')
+//         $('.sidebar').removeClass('open')
+//     })
+// })
