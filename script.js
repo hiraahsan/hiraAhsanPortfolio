7bc133624fe6cfc85826c6204b6bb144c538b3a2
Hira <hiraahsan94@gmail.com>
2020-05-30 01:34:42 -0400

    removing extraneous code

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
-// // @import Typed from 'typed.js';
-
 AOS.init();
 
 AOS.init({
     disable: "mobile",
 })
 
+
+
 $(document).ready(function () {
-    $('.navMenu').click(function () {
+    $('.navMenu').click( () => {
         $('.navMenu').toggleClass('open')
         $('.sidebar').toggleClass('open')
         $('nav').toggleClass('visuallyHidden')
 
     })
-    $('header a').click(function () {
+    $('header a').click( () => {
         $('.navMenu').removeClass('open')
         $('.sidebar').removeClass('open')
         $('nav').addClass('visuallyHidden')
+    })
+
+    $('form').submit( (e) => {
+        e.preventDefault();
+        console.log($('input'.value))
     })
 })
